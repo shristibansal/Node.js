d821b374d0b86e39d48e3610c5841690921bc7e9
shristibansal <shristibansal6@gmail.com>
2022-03-15 00:18:56 +0530

    notes-app

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,4 +1,16 @@
-const fs = require('fs')
+// const fs = require('fs')
+// fs.writeFileSync('notes.txt','My name is Shristi Bansal.')
+// fs.appendFileSync('notes.txt',' I live in India.')
 
-fs.writeFileSync('notes.txt','My name is Shristi Bansal.')
-fs.appendFileSync('notes.txt',' I live in India.')
+
+// const fname = require('./utils.js')
+
+// console.log(fname);
+
+// const add = require('./utils.js')
+// const sum = add(4,-2)
+// console.log(sum);
+
+const getNotes = require('./notes.js')
+const notes = getNotes()
+console.log(notes);
